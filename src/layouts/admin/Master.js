a807395c734ae984d54e0cb5c84429d80c5a0aaf
remonhasanapu <remon.hasan@technovista.com.bd>
2022-05-10 15:55:49 +0600

    added-navbar-for-admin

---
+++ src/layouts/admin/Master.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+export default function Master() {
+    return (
+        <div className='sb-nav-fixed'>
+            <Navbar/>
+        </div>
+    )
+}
